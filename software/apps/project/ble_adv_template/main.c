0e10557d37dc3dfdff95a0675a51d977e29b3cae
tomas <tomasscssantos@gmail.com>
2019-12-06 14:21:23 -0800

    working advertisement

--- software/apps/project/ble_adv_template/main.c
+++ software/apps/project/ble_adv_template/main.c
@@ -26,8 +26,8 @@ NRF_TWI_MNGR_DEF(twi_mngr_instance, 5, 0);
 static simple_ble_config_t ble_config = {
         // c0:98:e5:49:xx:xx
         .platform_id       = 0x49,    // used as 4th octect in device BLE address
-        .device_id         = 0x0000,  // TODO: replace with your lab bench number
-        .adv_name          = "EE149", // Note that this name is not displayed to save room in the advertisement for data.
+        .device_id         = 0x0002,  // TODO: replace with your lab bench number
+        .adv_name          = "Slave Kobuki", // Note that this name is not displayed to save room in the advertisement for data.
         .adv_interval      = MSEC_TO_UNITS(1000, UNIT_0_625_MS),
         .min_conn_interval = MSEC_TO_UNITS(500, UNIT_1_25_MS),
         .max_conn_interval = MSEC_TO_UNITS(1000, UNIT_1_25_MS),
